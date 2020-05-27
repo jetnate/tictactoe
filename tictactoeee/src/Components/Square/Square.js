1c414594d59f1bf18cf1b89b31f89700e770a5dd
Nathan Yordanos Tadesse <natetadesse4991@gmail.com>
2020-05-27 11:20:45 -0400

    added game

---
+++ tictactoeee/src/Components/Square/Square.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// Creating a component that will represent each square on my 
+// board.
+const Square = () => {
+    return (
+        <>
+            <button className="square">
+            </button>
+        </>
+    )
+}
+export default Square;
